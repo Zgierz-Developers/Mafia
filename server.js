4129dd6cefcbb4f55bd158d578e6b4542d9a96b2
Cushy <147443259+NoKrecik@users.noreply.github.com>
2025-01-05 18:40:13 +0100

    c

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const PORT = process.env.PORT || 80; // Zmieniono port na 8080
+const PORT = process.env.PORT || 8080; // Zmieniono port na 8080
 
 app.use(express.static(__dirname + '/public'));
 
