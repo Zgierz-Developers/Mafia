b99fe93986bf0d69cf7140ae98f212dd0ce3fd64
Cushy <147443259+NoKrecik@users.noreply.github.com>
2025-01-05 20:18:18 +0100

    k

--- server.js
+++ server.js
@@ -17,10 +17,6 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
-app.get('/test', (req, res) => {
-  res.send('Server is running!');
-});
-
 io.on('connection', (socket) => {
   console.log('New client connected');
 
