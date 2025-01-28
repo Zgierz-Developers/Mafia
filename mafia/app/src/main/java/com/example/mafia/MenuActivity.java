6ba0ecf2c66b7278ac0d19efc3fd89faadaca5dc
Czerwoo <czerwoo@onet.pl>
2025-01-28 19:07:49 +0100

    validate first run aplication, run settins on first time

--- mafia/app/src/main/java/com/example/mafia/MenuActivity.java
+++ mafia/app/src/main/java/com/example/mafia/MenuActivity.java
@@ -1,11 +1,15 @@
 package com.example.mafia;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "MyPrefs";
+    private static final String IS_FIRST_RUN = "isFirstRun";
 
     private Button serverListButton;
     private Button settingsButton;
@@ -13,7 +17,19 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean isFirstRun = sharedPreferences.getBoolean(IS_FIRST_RUN, true);
+
+        if (isFirstRun) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(IS_FIRST_RUN, false);
+            editor.apply();
+        } else {
+            setContentView(R.layout.activity_menu);
+        }
 
         serverListButton = findViewById(R.id.serverListButton);
         settingsButton = findViewById(R.id.settingsButton);
